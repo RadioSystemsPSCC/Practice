c0ba5a95960d79e0bc5e124ca40e100bec0f0885
DWalk <dwalker1@pstcc.edu>
2017-03-05 22:50:09 -0500

    Added blank ViewModels and added Gridview for Pets

---
+++ InvisibleFenceContract/InvisibleFenceContract/Viewmodels/Contact1ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvisibleFenceContract.Viewmodels
+{
+    class Contact1ViewModel
+    {
+    }
+}
