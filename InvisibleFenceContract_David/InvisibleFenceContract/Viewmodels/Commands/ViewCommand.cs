ab8e71c7b46f754dc1ba78825e70b44ff2bd2a46
David Oconnor <djoconnor@pstcc.edu>
2017-03-21 21:37:56 -0400

    new file with combined code

---
+++ InvisibleFenceContract_David/InvisibleFenceContract/Viewmodels/Commands/ViewCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace InvisibleFenceContract.Viewmodels.Commands
+{
+    class ViewCommand : ICommand
+    {
+        public Signature7ViewModel sigViewModel { get; set; }
+
+        public ViewCommand(Signature7ViewModel signatureVM)
+        {
+            this.sigViewModel = signatureVM;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            this.sigViewModel.ViewCommand();
+        }
+    }
+}
---
+++ InvisibleFenceContract_David/InvisibleFenceContract/Viewmodels/Contact1ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InvisibleFenceContract.Models;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace InvisibleFenceContract.Viewmodels
+{
+    class Contact1ViewModel : BindableBase
+    {
+        public List<Customer> c_Clients
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents()
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\docon\Documents\Capstone\text_multi_object.JSON");
+
+            List<Customer> cc_Clients = JsonConvert.DeserializeObject<List<Customer>>(text);
+
+            c_Clients = cc_Clients;
+        }
+
+    }
+}
