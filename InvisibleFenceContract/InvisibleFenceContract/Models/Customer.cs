d07c4901275fe0843cbf686617f44fe9c774c358
David Oconnor <djoconnor@pstcc.edu>
2017-03-26 14:04:09 -0400

    updated Customer model

--- InvisibleFenceContract/InvisibleFenceContract/Models/Customer.cs
+++ InvisibleFenceContract/InvisibleFenceContract/Models/Customer.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 
 namespace InvisibleFenceContract.Models
@@ -13,20 +16,24 @@ namespace InvisibleFenceContract.Models
         private string firstName;
         private string lastName;
         private string referredBy;
+        private string address;
+        private string city;
+        private string state;
+        private string zip;
+        private string homePhone;
+        private string cellPhone;
+        private string email;
+        private string petName;
+        private string petBreed;
+        private string petAge;
+
         /// <summary>
         /// Initiates a new instance of the customer class
         /// , String lastName, String address, String
         /// </summary>
 
-        public Customer()
-        {
-            {
-                this.FirstName = "David";
-                this.LastName = "Dog";
-                this.ReferredBy = "";
-            }
-        }
-      
+
+
         public String FirstName
         {
             get
@@ -66,5 +73,137 @@ namespace InvisibleFenceContract.Models
             }
         }
 
+        public String Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+                OnPropertyChanged("Address");
+            }
+        }
+
+        public String City
+        {
+            get
+            {
+                return city;
+            }
+            set
+            {
+                city = value;
+                OnPropertyChanged("City");
+            }
+        }
+
+        public String State
+        {
+            get
+            {
+                return state;
+            }
+            set
+            {
+                state = value;
+                OnPropertyChanged("State");
+            }
+        }
+
+        public String Zip
+        {
+            get
+            {
+                return zip;
+            }
+            set
+            {
+                zip = value;
+                OnPropertyChanged("Zip");
+            }
+        }
+
+        public String HomePhone
+        {
+            get
+            {
+                return homePhone;
+            }
+            set
+            {
+                homePhone = value;
+                OnPropertyChanged("HomePhone");
+            }
+        }
+
+        public String CellPhone
+        {
+            get
+            {
+                return cellPhone;
+            }
+            set
+            {
+                cellPhone = value;
+                OnPropertyChanged("CellPhone");
+            }
+        }
+
+        public String Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+
+        public String PetName
+        {
+            get
+            {
+                return petName;
+            }
+            set
+            {
+                petName = value;
+                OnPropertyChanged("PetName");
+            }
+        }
+
+        public String PetBreed
+        {
+            get
+            {
+                return petBreed;
+            }
+            set
+            {
+                petBreed = value;
+                OnPropertyChanged("PetBreed");
+            }
+        }
+
+        public String PetAge
+        {
+            get
+            {
+                return petAge;
+            }
+            set
+            {
+                petAge = value;
+                OnPropertyChanged("PetAge");
+            }
+
+        }
     }
 }
+
+
