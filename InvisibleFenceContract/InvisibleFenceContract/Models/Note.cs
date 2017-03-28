69bc1170c04cdeff01da4c516f3c3c9203ab8aa8
DWalk <dwalker1@pstcc.edu>
2017-03-28 09:35:41 -0400

    Added notes and pet class information and added Order class

---
+++ InvisibleFenceContract/InvisibleFenceContract/Models/Note.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvisibleFenceContract.Models
+{
+    class Note
+    {
+        private string customerID;
+        private string referralNoteType = "28";
+        private string referralNote;
+        private string lightingNoteType = "33";
+        private string lightingNote;
+        private string sprinklerNoteType = "33";
+        private string sprinklerNote;
+        private string presensitivitiesExistType = "33";
+        private string presensitivitesExist;
+        private string presensitivitiesNoteType = "33";
+        private string presensitivitesNotes;
+         
+        private string petBreed;
+        private string petBirthday;
+
+       // CustomerNote.NoteTypeID = '28'
+    }
+}
