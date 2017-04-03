2ee9c18abe21eae6dbb05740ddc84bf520036399
DWalk <dwalker1@pstcc.edu>
2017-04-03 13:12:51 -0400

    Added Product Model for Product Lookup Page

--- InvisibleFenceContract/InvisibleFenceContract/Models/Product.cs
+++ InvisibleFenceContract/InvisibleFenceContract/Models/Product.cs
@@ -8,5 +8,70 @@ namespace InvisibleFenceContract.Models
 {
     class Product
     {
+        private int partID;
+        private string partNumber;
+        private string partName;
+        private double partPrice;
+        private int isTaxable;
+
+        public int PartID
+        {
+            get
+            {
+                return partID;
+            }
+            set
+            {
+                partID = value;
+            }
+        }
+
+        public String PartNumber
+        {
+            get
+            {
+                return partNumber;
+            }
+            set
+            {
+                partNumber = value;
+            }
+        }
+
+        public String PartName
+        {
+            get
+            {
+                return partName;
+            }
+            set
+            {
+                partName = value;
+            }
+        }
+
+        public double PartPrice
+        {
+            get
+            {
+                return partPrice;
+            }
+            set
+            {
+                partPrice = value;
+            }
+        }
+
+        public int IsTaxable
+        {
+            get
+            {
+                return isTaxable;
+            }
+            set
+            {
+                isTaxable = value;
+            }
+        }
     }
 }
