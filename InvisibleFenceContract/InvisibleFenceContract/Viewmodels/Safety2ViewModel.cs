41a5093e3b07d474de19dd28d7645ae9201856ec
David Oconnor <djoconnor@pstcc.edu>
2017-04-22 18:22:24 -0400

    removed some using statements

--- InvisibleFenceContract/InvisibleFenceContract/Viewmodels/Safety2ViewModel.cs
+++ InvisibleFenceContract/InvisibleFenceContract/Viewmodels/Safety2ViewModel.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using InvisibleFenceContract.Models;
-using System.Collections.ObjectModel;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System.IO;
-using System.Windows;
-using System.Data;
-using InvisibleFenceContract.Services;
 
 namespace InvisibleFenceContract.Viewmodels
 {
